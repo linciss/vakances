c1a9dc1386f9fe334dfadc93b2b5f5559ba63acb
lincis <linardssils1@gmail.com>
2024-04-14 16:27:58 +0300

    little tweaks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
-import { Home } from './_root/index';
+import { Home } from './_root/pages/index';
+import { AuthLayout } from './_auth/AuthLayout';
+import { SignInForms } from './_auth/forms/index';
 
 function App() {
   return (
     <main>
       <Router>
         <Navbar />
-        <div className="m-auto">
+        <div className="m-auto max-w-[1280px]">
           <Routes>
+            <Route element={<AuthLayout />}>
+              <Route path="/login" element={<SignInForms />} />
+            </Route>
+
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
