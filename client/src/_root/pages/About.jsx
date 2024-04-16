886c31908ea2aa9a195e6778f11c38810380f1bc
lincis <linardssils1@gmail.com>
2024-04-16 20:21:28 +0300

    About page

--- client/src/_root/pages/About.jsx
+++ client/src/_root/pages/About.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const About = () => {
-  return <div className="container">About</div>;
+  return (
+    <div className="container">
+      <div className="flex justify-center mt-40 flex-col text-center gap-16">
+        <h1 className="text-7xl font-bold">IT ir spēks</h1>
+        <div>
+          <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            nobis, optio est vero excepturi tempore voluptates ipsa a
+            repellendus magnam, nihil quod, sapiente iste facere quo itaque
+            ratione modi nemo? Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Dignissimos eum ratione sequi quaerat minus maxime
+            vitae, repudiandae doloremque aspernatur fuga odio repellat libero,
+            laboriosam possimus nihil, in culpa quae architecto.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
