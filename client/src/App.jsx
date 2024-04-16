b955e3537bcdcc5f5754e760ca338578c860f108
lincis <linardssils1@gmail.com>
2024-04-16 22:05:28 +0300

    Wrapping app into ArticlesProvider for the articles caching

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,25 @@ import { Navbar } from './components/Navbar';
 import { About, Contact, Home, Vacancies } from './_root/pages/index';
 import { AuthLayout } from './_auth/AuthLayout';
 import { SignInForms } from './_auth/forms/index';
+import { ArticlesProvider } from './context/ArticlesContext';
 
 function App() {
   return (
-    <main>
+    <main className="bg-lightGreen/20 min-h-screen">
       <Navbar />
-      <div className="m-auto max-w-[1280px]">
-        <Routes>
-          {/* PUBLIC ROUTES  */}
-          <Route element={<AuthLayout />}>
-            <Route path="/login" element={<SignInForms />} />
-          </Route>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/vacancies" element={<Vacancies />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+      <div className="m-auto max-w-[1280px] ">
+        <ArticlesProvider>
+          <Routes>
+            {/* PUBLIC ROUTES  */}
+            <Route element={<AuthLayout />}>
+              <Route path="/login" element={<SignInForms />} />
+            </Route>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/vacancies" element={<Vacancies />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </ArticlesProvider>
       </div>
     </main>
   );
