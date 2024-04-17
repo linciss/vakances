081092d226138813f83a433bb1e1a39bd103f065
lincis <linardssils1@gmail.com>
2024-04-17 23:01:06 +0300

    A simple authentication using sessions :)

---
+++ backend/routes/auth.js
@@ -0,0 +1,70 @@
+import express from 'express';
+
+const router = express.Router();
+
+const requireAuth = (req, res, next) => {
+  const { user } = req.session;
+
+  if (!user) {
+    return res.status(401).send('Jūs neesat autorizējies!');
+  }
+  next();
+};
+
+const requireAdmin = (req, res, next) => {
+  const { user } = req.session;
+
+  if (user.role !== 'root') {
+    return res.status(401).send('Jūs neesat autorizējies!');
+  }
+  next();
+};
+
+const user = {
+  username: 'admin',
+  password: 'admin',
+  role: 'root',
+};
+
+router.post('/login', (req, res) => {
+  console.log(req.body);
+
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(401).send('Nepareizs lietotājvārds vai parole!');
+  }
+
+  if (password === user.password && username === user.username) {
+    req.session.authenticated = true;
+
+    req.session.user = {
+      username,
+      role: user.role,
+    };
+
+    res.status(200).json({ message: 'Logged in' });
+    console.log(req.session, 'logged in!');
+  } else {
+    res.status(401).send('Nepareizs lietotājvārds vai parole!');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send('Server error');
+    }
+    console.log('logged out!', req.session);
+    res.clearCookie(process.env.SESSION_NAME);
+    res.status(200).send('Logged out');
+  });
+});
+
+// check whether user is authenticated
+router.get('/user', requireAuth, (req, res) => {
+  console.log('user is authenticated!');
+  res.status(200).json(req.session.user);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,35 @@ import helmet from 'helmet';
 const app = express();
 import newsRouter from './routes/news.js';
 import mongoose from 'mongoose';
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+import auth from './routes/auth.js';
 
+app.use(express.json());
 app.use(
   cors({
     origin: 'http://localhost:5173',
   })
 );
 app.use(helmet());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      dbName: process.env.DB_NAME,
+    }),
+  })
+);
 
 app.use('/api/news', newsRouter);
+app.use('/api/auth', auth);
 
-app.get('/', async (req, res) => {});
+app.get('/', async (req, res) => {
+  res.send('Hello World!');
+});
 
 mongoose
   .connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME })
