e9ff92751601c9543cfab705a980de8da89215f9
lincis <linardssils1@gmail.com>
2024-04-19 23:13:18 +0300

    making change-password  endpoint so users can change password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,8 +12,16 @@ const saltRounds = 10;
 router.post('/register', requireAdmin, async (req, res) => {
   const { username, password, role } = req.body;
 
-  if (!username || !password) {
-    return res.status(401).send('Lietotājvārds un parole ir obligāti!');
+  if (!username || !password || !role) {
+    return res.status(401).send('Ievadiet visus laukus!');
+  }
+
+  if (role !== 'moderator' && role !== 'root' && role !== 'admin') {
+    return res.status(401).send('Nepareiza lietotāja loma!');
+  }
+
+  if (role === 'root') {
+    return res.status(401).send('Jums nav atļaujas izveidot šādu lietotāju!');
   }
 
   //check whether user exists
@@ -63,6 +71,7 @@ router.post('/login', async (req, res) => {
       req.session.user = {
         username,
         role: user.role,
+        timeCreated: user.timeCreated,
       };
 
       res.status(200).json(req.session.user);
@@ -84,6 +93,86 @@ router.get('/logout', (req, res) => {
     res.clearCookie(process.env.SESSION_NAME);
     res.status(200).send('Logged out');
   });
+});
+
+router.put('/change-password', requireAuth, async (req, res) => {
+  const { oldPassword, newPassword, newPasswordVerify } = req.body;
+  const { user } = req.session;
+
+  if (!oldPassword || !newPassword || !newPasswordVerify) {
+    return res.status(401).send('Ievadiet visus laukus!');
+  }
+
+  if (newPassword !== newPasswordVerify) {
+    return res.status(401).send({ error: 'passwords_do_not_match' });
+  }
+
+  try {
+    const dbUser = await User.findOne({ username: user.username });
+
+    const match = await bcrypt.compare(oldPassword, dbUser.password);
+
+    if (!match) {
+      return res.status(401).send({ error: 'incorrect_password' });
+    }
+
+    if (oldPassword === newPassword) {
+      return res.status(401).send({ error: 'passwords_are_the_same' });
+    }
+
+    const hash = await bcrypt.hash(newPassword, saltRounds);
+
+    await User.updateOne({ username: dbUser.username }, { password: hash });
+
+    res.status(200).send('Parole nomainīta!');
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Kļūda ienākošajos datus!');
+  }
+});
+
+router.put('/change-username', requireAuth, async (req, res) => {
+  const { newUsername, password } = req.body;
+  const { user } = req.session;
+
+  if (!newUsername) {
+    return res.status(401).send({ error: 'fill_all_fields' });
+  }
+
+  if (newUsername === user.username) {
+    return res.status(401).send({ error: 'username_is_the_same' });
+  }
+
+  try {
+    const dbUser = await User.findOne({ username: user.username });
+
+    // check whether user exists
+    const userExists = await User.findOne({
+      username: { $regex: new RegExp(`^${newUsername}$`, 'i') },
+    });
+
+    if (userExists) {
+      return res.status(401).send({ error: 'user_exists' });
+    }
+
+    const match = await bcrypt.compare(password, dbUser.password);
+
+    if (!match) {
+      return res.status(401).send({ error: 'incorrect_password' });
+    }
+
+    await User.updateOne(
+      { username: dbUser.username },
+      { username: newUsername }
+    );
+
+    req.session.user.username = newUsername;
+
+    res.status(200).json(req.session.user);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Kļūda ienākošajos datus!');
+  }
 });
 
 // check whether user is authenticated
