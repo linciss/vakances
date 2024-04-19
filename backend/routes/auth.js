f84919b5fef253e289a991a67051d193bdafa50c
lincis <linardssils1@gmail.com>
2024-04-19 17:53:54 +0300

    adding a proper login and register endpoint that actually stores and gets users from database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,52 +1,80 @@
 import express from 'express';
+import { User } from '../schemas/userSchema.js';
+import bcrypt from 'bcrypt';
+import { requireAdmin, requireAuth } from '../auth/authMiddleWare.js';
 
 const router = express.Router();
+const saltRounds = 10;
 
-const requireAuth = (req, res, next) => {
-  const { user } = req.session;
+// MIDDLEWARE TO CHECK IF USER IS AUTHENTICATED
 
-  if (!user) {
-    return res.status(401).send('Jūs neesat autorizējies!');
+// LOGIN AND REGISTER AND SO ON
+
+// need to authorise user later :)) PLEASE DONT FORGET
+router.post('/register', requireAdmin, async (req, res) => {
+  const { username, password, role } = req.body;
+
+  if (!username || !password) {
+    return res.status(401).send('Lietotājvārds un parole ir obligāti!');
   }
-  next();
-};
 
-const requireAdmin = (req, res, next) => {
-  const { user } = req.session;
+  //check whether user exists
+  const user = await User.findOne({ username });
 
-  if (user.role !== 'root') {
-    return res.status(401).send('Jūs neesat autorizējies!');
+  if (user) {
+    return res.status(401).send('Lietotājs jau eksistē!');
   }
-  next();
-};
 
-const user = {
-  username: 'admin',
-  password: 'admin',
-  role: 'root',
-};
+  try {
+    const hash = await bcrypt.hash(password, saltRounds);
 
-router.post('/login', (req, res) => {
-  console.log(req.body);
+    const newUser = new User({
+      username,
+      password: hash,
+      role: role,
+    });
 
+    await newUser.save();
+
+    res.status(200).send('Lietotājs saglabāts!');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Kļūda saglabājot lietotāju!');
+  }
+});
+
+router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
     return res.status(401).send('Nepareizs lietotājvārds vai parole!');
   }
 
-  if (password === user.password && username === user.username) {
-    req.session.authenticated = true;
+  try {
+    const user = await User.findOne({ username });
 
-    req.session.user = {
-      username,
-      role: user.role,
-    };
+    if (!user) {
+      return res.status(401).send('Nepareizs lietotājvārds vai parole!');
+    }
 
-    res.status(200).json({ message: 'Logged in' });
-    console.log(req.session, 'logged in!');
-  } else {
-    res.status(401).send('Nepareizs lietotājvārds vai parole!');
+    const match = await bcrypt.compare(password, user.password);
+
+    if (match) {
+      req.session.authenticated = true;
+
+      req.session.user = {
+        username,
+        role: user.role,
+      };
+
+      res.status(200).json(req.session.user);
+      console.log(req.session, 'logged in!');
+    } else {
+      res.status(401).send('Nepareizs lietotājvārds vai parole!');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Kļūda ienākošajos datus!');
   }
 });
 
@@ -55,7 +83,6 @@ router.get('/logout', (req, res) => {
     if (err) {
       return res.status(500).send('Server error');
     }
-    console.log('logged out!', req.session);
     res.clearCookie(process.env.SESSION_NAME);
     res.status(200).send('Logged out');
   });
