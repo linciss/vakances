f8379cb8ce3197bf14c5e85f8b8264427ba259ac
lincis <linardssils1@gmail.com>
2024-04-19 23:14:55 +0300

    Profile page

---
+++ client/src/_root/private/Profile.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import userProfile from '../../assets/userProfileImage.png';
+import { AuthContext } from '../../context/AuthContext';
+import { Link } from 'react-router-dom';
+
+const Profile = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="container ">
+      <div className="m-auto flex justify-center mt-20 flex-wrap gap-8">
+        <img src={userProfile} className="w-1/3 " />
+        <div className="flex flex-col gap-4">
+          <h1 className="font-bold text-4xl">Vārds: {user.username}</h1>
+          <h2 className="font-bold text-2xl">Privilēģijas: {user.role}</h2>
+          <h2 className="font-bold text-2xl">
+            Konts taisīts: {user.timeCreated.slice(0, 10)}
+          </h2>
+          <div className="flex gap-4 ">
+            <Link to="/profile/change-username">
+              <button className="bg-mainGreen text-white p-2 rounded-md">
+                Mainīt lietotājvārdu
+              </button>
+            </Link>
+            <Link to="/profile/change-password">
+              <button className="bg-mainGreen text-white p-2 rounded-md">
+                Mainīt paroli
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
