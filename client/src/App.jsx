26dcf0a139fb4775545bf4fb7a2b4ea6c97d3b2c
lincis <linardssils1@gmail.com>
2024-04-20 19:42:53 +0300

    removing routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+
 import { Navbar } from './components/Navbar';
-import { About, Contact, Home, Vacancies, Profile } from './_root/pages/index';
-import { AuthLayout } from './_auth/AuthLayout';
-import { PasswordForms, SignInForms, UsernameForms } from './_auth/forms/index';
+
 import { ArticlesProvider } from './context/ArticlesContext';
 import { AuthProvider } from './context/AuthContext';
-import { PrivateRoutes } from './_auth/PrivateRoutes';
+import { Views } from './routes/Views';
 
 function App() {
   return (
@@ -15,29 +13,7 @@ function App() {
         <Navbar />
         <div className="m-auto max-w-[1280px] ">
           <ArticlesProvider>
-            <Routes>
-              {/* PUBLIC ROUTES  */}
-              <Route element={<AuthLayout />}>
-                <Route path="/login" element={<SignInForms />} />
-              </Route>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/vacancies" element={<Vacancies />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-
-              {/* PRIVATE ROUTES */}
-              <Route element={<PrivateRoutes />}>
-                <Route path="/profile" element={<Profile />} />
-                <Route
-                  path="/profile/change-password"
-                  element={<PasswordForms />}
-                />
-                <Route
-                  path="/profile/change-username"
-                  element={<UsernameForms />}
-                />
-              </Route>
-            </Routes>
+            <Views />
           </ArticlesProvider>
         </div>
       </main>
