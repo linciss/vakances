15e5ac813080075abd0279be241e38f621023b4b
lincis <linardssils1@gmail.com>
2024-05-09 20:12:11 +0300

    changes to header still bad lol

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,22 +11,22 @@ export const Header = () => {
       }}
     >
       <div className="hero-overlay bg-opacity-80 "></div>
-      <div className="hero-content flex-col justify-start items-start">
-        <div className="text-white flex">
-          <div className="flex flex-col justify-center items-start gap-4">
-            <h1 className="text-7xl md:text-8xl font-bold md:text-start text-center">
+      <div className="hero-content flex-col justify-start items-start mx-8 xl:mx-0">
+        <div className="text-white lg:flex grid grid-cols-1">
+          <div className="flex items-center justify-center lg:items-start gap-4 flex-col">
+            <h1 className="text-7xl md:text-8xl font-bold md:text-start text-center ">
               IT ir spēks
             </h1>
-            <div className="w-full md:w-1/2 flex flex-col justify-center items-start gap-4">
-              <p className="py-6 md:text-2xl text-xl text-center md:text-start">
+            <div className="w-full md:w-1/2 flex flex-col justify-center items-center lg:items-start gap-4 ">
+              <p className="py-6 md:text-2xl text-xl text-center lg:text-start">
                 Mēs esam šeit, lai palīdzētu Jums izprast IT nozares
                 aktualitātes un piedāvāt jaunākās IT vakances.
               </p>
-              <button className="btn btn-primary">Atrast vakances</button>
+              <button className="btn btn-primary ">Atrast vakances</button>
             </div>
           </div>
           <div>
-            <img src={headerVector} />
+            <img className="lg:block hidden" src={headerVector} />
           </div>
         </div>
       </div>
