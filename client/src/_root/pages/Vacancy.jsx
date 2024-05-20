984e7546629bad118da7fb1ea2ffb82845ecf682
linciss <122517407+linciss@users.noreply.github.com>
2024-05-20 09:14:09 +0000

    new sahit for vacancy info

--- client/src/_root/pages/Vacancy.jsx
+++ client/src/_root/pages/Vacancy.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { LocationSVG } from '../../assets/LocationIcon';
+import { MoneySVG } from '../../assets/Money';
+import { LoadSVG } from '../../assets/Load';
+import { TimeSVG } from '../../assets/Time';
 
 export const Vacancy = () => {
   const { id } = useParams();
@@ -25,14 +29,90 @@ export const Vacancy = () => {
 
   return (
     <div className="container max-w-[1280px]">
-      <div>{vacancy.title}</div>
+      {/* <div>{vacancy.title}</div>
       <div>{vacancy.description}</div>
       <div>{vacancy.address}</div>
       <div>{vacancy.salary}</div>
       <div>{vacancy.experience}</div>
       <div>{vacancy.workTime}</div>
       <div>{vacancy.workType}</div>
-      <div>{vacancy.load}</div>
+      <div>{vacancy.load}</div> */}
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:text-center">
+            <p className="mt-2 text-3xl font-bold tracking-tight  sm:text-4xl">
+              {vacancy.title}
+            </p>
+            <p className="mt-6 text-lg leading-8 ">{vacancy.description}</p>
+          </div>
+          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+              <div className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 text-">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600"></div>
+                  Darba pieredze:
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {vacancy.experience}
+                </dd>
+              </div>
+              <div className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 ">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-neutral">
+                    <TimeSVG />
+                  </div>
+                  Darba laiks:
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {vacancy.workTime}
+                </dd>
+              </div>
+              <div className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 ">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600"></div>
+                  Darba veids:
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {vacancy.workType}
+                </dd>
+              </div>
+              <div className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 ">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-neutral">
+                    <MoneySVG className="w-2 h-2" />
+                  </div>
+                  Alga (bruto):
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {vacancy.salary} €
+                </dd>
+              </div>
+              <div className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 ">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-neutral">
+                    <LocationSVG />
+                  </div>
+                  Adrese:
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {vacancy.address}
+                </dd>
+              </div>
+              <div className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 ">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-neutral">
+                    <LoadSVG />
+                  </div>
+                  Darba slodze:
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {vacancy.load}
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
