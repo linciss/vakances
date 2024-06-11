e9368b7f5ad9eee318d7482f7bf6692bbc8001e4
lincis <linardssils1@gmail.com>
2024-06-11 22:53:30 +0300

    checks plus users are editable

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -3,9 +3,10 @@ import {
   downloadFile,
   getFiles,
   uploadFile,
-} from '../controllers/FIleController.js';
+} from '../controllers/FileController.js';
 import { File } from '../schemas/fileSchema.js';
 import multer from 'multer';
+import { requireAuth } from '../auth/requireAuth.js';
 
 const router = express.Router();
 
@@ -13,8 +14,8 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 // router.post('/upload', uploadFile);
-router.post('/upload', upload.single('file'), uploadFile);
-router.get('/download/:id', downloadFile);
-router.get('/get/:id', getFiles);
+router.post('/upload', requireAuth, upload.single('file'), uploadFile);
+router.get('/download/:id', requireAuth, downloadFile);
+router.get('/get/:id', requireAuth, getFiles);
 
 export default router;
