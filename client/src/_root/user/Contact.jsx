8f40a098f27b211884637147cadb581fead61f43
lincis <linardssils1@gmail.com>
2024-06-12 16:47:13 +0300

    can email me now 8)

--- client/src/_root/user/Contact.jsx
+++ client/src/_root/user/Contact.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { PhoneIcon } from '../../assets/PhoneIcon';
 import { PaperPlaneIcon } from '../../assets/PaperPlaneIcon';
 
 import { LocationPinIcon } from '../../assets/LocationPinIcon';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 const contactTab = [
   {
@@ -24,6 +27,42 @@ const contactTab = [
 ];
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    if (isSubmitting) return;
+    setIsSubmitting(true);
+    await axios
+      .post('/api/mail/send', data)
+      .catch((err) => {
+        console.log(err);
+      })
+      .then((res) => {
+        if (!res || res.status !== 200) {
+          return;
+        }
+        return res.data;
+      })
+      .then((data) => {
+        if (!data) {
+          return;
+        }
+        setSuccess('Ziņojums aizsūtīts!');
+        setTimeout(() => {
+          navigate('/');
+          setIsSubmitting(false);
+        }, 2000);
+      });
+  };
   return (
     <div className="pb-1 ">
       <div className="w-full text-center py-12  flex flex-col justify-center items-center">
@@ -55,8 +94,51 @@ const Contact = () => {
               iespējas ātrāk.
             </p>
           </div>
-          <form className=" rounded-lg md:mt-0 mt-8 ">
+          <form
+            className=" rounded-lg md:mt-0 mt-8 "
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div className="flex flex-col">
+              {error ? (
+                <div role="alert" className="alert alert-error">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="stroke-current shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  <span className="text-white">
+                    Lūdzu aizpildiet visus obligātos laukus
+                  </span>
+                </div>
+              ) : null}
+              {success ? (
+                <div role="alert" className="alert alert-success">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="stroke-current shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  <span>{success}</span>
+                </div>
+              ) : (
+                ''
+              )}
               <div className="mx-auto form-control w-full">
                 <label className="label">
                   <span className="label-text">Pilnais vārds</span>
@@ -64,6 +146,7 @@ const Contact = () => {
                 <input
                   type="text"
                   className="input input-bordered w-full bg-white"
+                  {...register('name', { required: true })}
                 />
               </div>
               <div className="mx-auto form-control w-full">
@@ -73,6 +156,7 @@ const Contact = () => {
                 <input
                   type="text"
                   className="input input-bordered w-full bg-white"
+                  {...register('phone', { required: true })}
                 />
               </div>
               <div className="mx-auto form-control w-full">
@@ -82,6 +166,7 @@ const Contact = () => {
                 <input
                   type="text"
                   className="input input-bordered w-full bg-white"
+                  {...register('email', { required: true })}
                 />
               </div>
               <label className="form-control w-full m-auto">
@@ -92,6 +177,7 @@ const Contact = () => {
                   id="description"
                   className="textarea textarea-bordered h-36 resize-none w-full bg-white"
                   required
+                  {...register('message', { required: true })}
                 ></textarea>
               </label>
               <div className="w-full my-4 flex justify-end">
